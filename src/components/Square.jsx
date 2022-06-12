d2e8d6798096f7e95af5677b260d99eccbb9ada6
Harshida Shaily <harshidashaily@gmail.com>
2022-06-13 02:13:14 +0530

    added saas,get to know styles

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Square = ({ value, children }) => {
-  return <button type="button">{children}</button>;
+const Square = props => {
+  console.log(props);
+  return (
+    <button type="button" className="square">
+      {props.value}
+    </button>
+  );
 };
 
 export default Square;

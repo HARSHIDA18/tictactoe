d8def5992cfa1d1bce393761dc3069d8c395ce45
Harshida Shaily <harshidashaily@gmail.com>
2022-06-12 18:24:59 +0530

    created board square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-
+import React from 'react';
+import Board from './components/Board';
 const App = () => {
   return (
     <>
-      <h1>Welcome to React Vite Micro App!</h1>
-      <p>Hii My Name Is Harshida Shaily</p>
-      <small>hello this is me</small>
+      <h1>TIC TAC TOE</h1>
+      <Board />
     </>
   );
 };
